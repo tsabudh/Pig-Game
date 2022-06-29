d1add9ac8b667d2ea529e9156944d9269cd67070
Sabudh Bahadur Thapa <tsabudh@gmail.com>
2022-06-29 14:21:06 +0545

    changing active players
    toggling active class betwn players

--- script.js
+++ script.js
@@ -6,7 +6,13 @@ const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn-hold');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
 
+let currentScore = 0;
+let activeplayer = 0;
 
 
 //Initial score is 0
@@ -24,4 +30,26 @@ btnRoll.addEventListener('click', function()
     let dice = Math.trunc(Math.random()*6 + 1);
     diceEl.src = `dice-${dice}.png`;
     diceEl.classList.remove('hidden');
+    
+    
+    //If dice is not 1 add the dice to score
+
+
+    if(dice != 1)
+    {
+        currentScore += dice;
+        document.getElementById(`current--${activeplayer}`).textContent = currentScore;
+        
+    }else{
+
+        currentScore = 0; 
+        document.getElementById(`current--${activeplayer}`).textContent = currentScore;
+        activeplayer = activeplayer === 0 ? 1: 0;
+        player0.classList.toggle('player--active');
+        player1.classList.toggle('player--active');
+
+    }
+   
 });
+
+
